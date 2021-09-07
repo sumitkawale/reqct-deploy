db4d3f6057569107a39d08430c47c001fc976a90
sumitkawale <sumitkawale2002@gmail.com>
2021-09-07 16:27:22 +0530

    deploy by react app to github pages

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+function App() {
+    return (<>
+        <h1 style={{color:'blueviolet', padding:"5rem", textAlign:"center", fontSize:'4rem'}}>This is my first deployment of react app 😊</h1>
+    </>)
+}
+export default App
